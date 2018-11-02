951558261dd02272501e9beb95888bd284f2c60b
Cristian Lucero <cristianjlucero32@gmail.com>
2018-11-02 10:44:26 -0700

    This commit adds pizza constructor and a prototype to determine the price.

---
+++ js/scripts.js
@@ -0,0 +1,42 @@
+// Backend
+var pizzaPrice = 0;
+
+
+        //Pizza Constructor
+function MyPizza (pizzaSize, pizzaTopping) {
+    this.pizzaSize = pizzaSize,
+    this.pizzaTopping = pizzaTopping,
+    this.pizzaPrice = 0;
+}
+
+
+        //Prototype for determing the customer's prices based off selections.
+MyPizza.prototype.getPrice = function() {
+    if (this.pizzaSize === 'small') {
+        this.pizzaPrice = this.pizzaTopping.length + 8
+        return this.pizzaPrice 
+    }
+    else if (this.pizzaSize === 'medium') {        
+        this.pizzaPrice = this.pizzaTopping.length + 11         
+        return this.pizzaPrice
+    }
+    else {
+        this.pizzaPrice = this.pizzaTopping.length + 13 
+        return this.pizzaPrice
+    }
+}
+
+
+
+
+
+
+
+
+// Front End
+$(document).ready(function() {
+    event.preventDefault();
+
+
+
+});
