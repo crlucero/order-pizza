7471c3c84d576c3d31af1ae070d1b6a92658b2d8
Cristian Lucero <cristianjlucero32@gmail.com>
2018-11-02 17:31:51 -0700

    This commit will update the formula for prices. Still can't pull size value

--- js/scripts.js
+++ js/scripts.js
@@ -5,6 +5,7 @@ var pizzaPrice = {
     large: "large"
 }
 
+
         //Pizza Constructor
 function Pizza (pizzaSize, pizzaTopping, ) {
     this.pizzaSize = pizzaSize;
@@ -13,42 +14,37 @@ function Pizza (pizzaSize, pizzaTopping, ) {
 
         //Prototype for determing the customer's prices based off selections.
 Pizza.prototype.getPrice = function() {
+    
     if (this.pizzaSize = "small") {
-        this.pizzaPrice = this.pizzaTopping +8;
+        this.pizzaPrice = this.pizzaTopping.length +8;
         return this.pizzaPrice;
     }
-    else if (this.pizzaSize = "medium") {        
-        this.pizzaPrice = this.pizzaTopping +10;         
-        return this.pizzaPrice;
+    else if  (this.pizzaSize = "medium") {        
+        this.pizzaPrice = this.pizzaTopping.length +10;         
+        return pizzaPrice;
     }
     else {
-        this.pizzaPrice = this.pizzaTopping +12; 
-        return this.pizzaPrice;
+        this.pizzaPrice = this.pizzaTopping.length +12; 
+        return pizzaPrice;
     }
 }
 
-
-
-
-
-
-
-
 // Front End
 $(document).ready(function() {
-$("form#form").submit(function(event) {
-    event.preventDefault();
 
-    var size = $("input#pizzaSize").children("option:selected").val();
+    $("form#form").submit(function(event) {
+        event.preventDefault();
+        var pizzaSize = $("select#pizzaSize").children("option:selected").val();
+        var pizzaTopping = [' '];
+        $("input[type=checkbox][name=toppings]:checked").each(function(){            
+            pizzaTopping.push($(this).val());
+        });
     
-    var pizzaTopping = [' '];
-    $("input[type=checkbox][name=toppings]:checked").each(function(){            
-        pizzaTopping.push($(this).val());
-    });
-    
-    var myPizza = new Pizza (size, pizzaTopping, pizzaPrice);
+    var myPizza = new Pizza (pizzaSize, pizzaTopping, pizzaPrice);
         myPizza.getPrice();
 
-            $("p#cost").text("Your order has been confirmed. You ordered a " + myPizza.size + " pizza with" + myPizza.pizzaTopping + " as your toppings." + " Your total will be $"+myPizza.pizzaPrice);
+            $("p#cost").text("Your order has been confirmed. You ordered a " 
+            + myPizza.pizzaSize + " pizza with" + myPizza.pizzaTopping + " as your toppings." + 
+            " Your total will be $" + myPizza.pizzaPrice);
     });
 });
