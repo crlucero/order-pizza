61ed84e5c0bffa3a37854a0d15f12a7d45bef695
Cristian Lucero <cristianjlucero32@gmail.com>
2018-11-04 13:35:39 -0800

    This commit updates scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,32 +1,32 @@
 // Backend
-var pizzaPrice = {
-    small: "small",
-    medium: "medium",
-    large: "large"
-}
 
+var pizzaPrice = 0;
 
-        //Pizza Constructor
-function Pizza (pizzaSize, pizzaTopping, ) {
+function Pizza (pizzaSize, pizzaTopping) {
     this.pizzaSize = pizzaSize;
     this.pizzaTopping = pizzaTopping;
+    this.pizzaPrice = 0;
 }
 
         //Prototype for determing the customer's prices based off selections.
 Pizza.prototype.getPrice = function() {
-    
-    if (this.pizzaSize = "small") {
-        this.pizzaPrice = this.pizzaTopping.length +8;
-        return this.pizzaPrice;
-    }
-    else if  (this.pizzaSize = "medium") {        
-        this.pizzaPrice = this.pizzaTopping.length +10;         
-        return pizzaPrice;
-    }
-    else {
-        this.pizzaPrice = this.pizzaTopping.length +12; 
-        return pizzaPrice;
-    }
+
+    if (this.pizzaSize === "small") {
+        this.pizzaPrice = this.pizzaTopping.length +8
+        return this.pizzaPrice
+     }
+     else if  (this.pizzaSize === "medium") {        
+         this.pizzaPrice = this.pizzaTopping.length +10     
+         return pizzaPrice
+     }
+     else if (this.pizzaSize === "Large"){
+         this.pizzaPrice = this.pizzaTopping.length +12
+         return this.pizzaPrice
+     }
+     else {
+        //  alert("You did not select a size. Please choose the size of pizza you would like to order.")
+         
+     }
 }
 
 // Front End
@@ -34,13 +34,14 @@ $(document).ready(function() {
 
     $("form#form").submit(function(event) {
         event.preventDefault();
+        
         var pizzaSize = $("select#pizzaSize").children("option:selected").val();
         var pizzaTopping = [' '];
         $("input[type=checkbox][name=toppings]:checked").each(function(){            
             pizzaTopping.push($(this).val());
         });
     
-    var myPizza = new Pizza (pizzaSize, pizzaTopping, pizzaPrice);
+    var myPizza = new Pizza(pizzaSize, pizzaTopping, pizzaPrice);
         myPizza.getPrice();
 
             $("p#cost").text("Your order has been confirmed. You ordered a " 
